6c98825cc19870bdade1c14024f33dadf985adde
declan oller <declanoller@gmail.com>
2025-05-24 16:26:44 -0400

    renamed file

--- _posts/2025-05-24-Hello blogness, my old friend.md
+++ _posts/2025-05-24-Hello blogness, my old friend.md
@@ -1,5 +1,5 @@
 ---
-date: 2025-05-23 00:00:00-05:00
+date: 2025-05-24 00:00:00-05:00
 layout: post
 permalink: 2025-05-24-hello-blogness
 thumbnail: /assets/images/thumbnails/iaintreadingallthat_thumb.jpg
---
+++ misc/resize_thumbnails.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from PIL import Image
+from typing import Optional, Tuple
+from termcolor import colored
+
+FILESIZE_THRESHOLD_KB = 500
+
+
+def get_image_info(file_path: str) -> Optional[Tuple[float, int, int]]:
+    try:
+        with Image.open(file_path) as img:
+            width, height = img.size
+            file_size_kb = os.path.getsize(file_path) / 1024  # Convert bytes to kB
+            return file_size_kb, width, height
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+        return None
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: python resize_thumbnails.py <directory_path>")
+        sys.exit(1)
+
+    directory_path = sys.argv[1]
+
+    if not os.path.isdir(directory_path):
+        print(f"Error: {directory_path} is not a valid directory.")
+        sys.exit(1)
+
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            if file.lower().endswith((".png", ".jpg", ".jpeg")):
+                file_path = os.path.join(root, file)
+                image_info = get_image_info(file_path)
+                if image_info:
+                    file_size_kb, width, height = image_info
+                    if file_size_kb > FILESIZE_THRESHOLD_KB:
+                        print(colored(f"{file}", "red"))
+                        print(
+                            colored(
+                                f"    {file_size_kb:.2f} kB, {width}x{height}", "red"
+                            )
+                        )
+                    else:
+                        print(f"{file}")
+                        print(f"    {file_size_kb:.2f} kB, {width}x{height}")
+
+
+if __name__ == "__main__":
+    main()
